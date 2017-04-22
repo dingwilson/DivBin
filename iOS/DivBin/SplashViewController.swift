d15edfefa2ba94ec9ae5196b7fe770e356c4f042
Wilson Ding <shodai100@gmail.com>
2017-04-22 03:10:40 -0500

    Added UI for Splash

--- iOS/DivBin/SplashViewController.swift
+++ iOS/DivBin/SplashViewController.swift
@@ -7,29 +7,16 @@
 //
 
 import UIKit
+import SwiftVideoBackground
 
 class SplashViewController: UIViewController {
 
+    @IBOutlet weak var backgroundVideo: BackgroundVideo!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        backgroundVideo.createBackgroundVideo(name: "Flying-Birds", type: "mp4")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
