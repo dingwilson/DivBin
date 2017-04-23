8e7adcd74f68e89e5c467fe62d77f535fdf55be3
Wilson Ding <shodai100@gmail.com>
2017-04-23 03:10:57 -0500

    Modified Authentication Code

--- iOS/DivBin/ProfileViewController.swift
+++ iOS/DivBin/ProfileViewController.swift
@@ -7,29 +7,211 @@
 //
 
 import UIKit
+import Eureka
+import Firebase
+import FirebaseDatabase
+import GoogleSignIn
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ProfileViewController: UIViewController {
+enum AuthProvider {
+    case authFacebook
+    case authGoogle
+}
 
+class ProfileViewController: FormViewController, GIDSignInUIDelegate {
+    
+    var savedImage : UIImage?
+    
+    var savedUsername : String = ""
+    var savedEmail : String = ""
+    var savedUID : String = ""
+    
+    var isAuthViaFacebook : Bool = false
+    var isAuthViaGoogle : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        getData()
+        
+        buildForm()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        getData()
     }
-    */
+    
+    func getData() {
+        if let user = FIRAuth.auth()?.currentUser {
+            if let email = user.email {
+                self.savedEmail = email // Get User Email
+            }
+            
+            self.savedUID = user.uid // Get User UID
+            
+            let ref = FIRDatabase.database().reference()
+            
+            // Get User Username
+            ref.child("users/\(self.savedUID)/public/username").observeSingleEvent(of: .value, with: { (snapshot) in
+                guard let username = snapshot.value as? String else {
+                    return
+                }
+                
+                self.savedUsername = username
+                
+                self.form.rowBy(tag: "usernameRow")?.updateCell()
+                self.tableView?.reloadData()
+            })
+            
+            // Get User Photo
+            let photoURL = user.photoURL
+            
+            struct last {
+                static var photoURL : URL? = nil
+            }
+            
+            last.photoURL = photoURL
+            
+            if let photoURL = photoURL {
+                let data = try? Data.init(contentsOf: photoURL)
+                if let data = data {
+                    let image = UIImage.init(data: data)
+                    
+                    if photoURL == last.photoURL {
+                        self.savedImage = image
+                    }
+                }
+            } else {
+                self.savedImage = UIImage(named: "GenericProfilePhoto")
+            }
+            
+            // Get User Provider List
+            let userProviders = FIRAuth.auth()?.currentUser?.providerData
+            
+            for provider in userProviders! {
+                if provider.providerID == FIRFacebookAuthProviderID {
+                    self.isAuthViaFacebook = true
+                } else if provider.providerID == FIRGoogleAuthProviderID {
+                    self.isAuthViaGoogle = true
+                }
+            }
+            
+            self.tableView?.reloadData()
+            
+        } else {
+            self.createAlert(title: "Error", message: "Oops, you don't seem to be signed in...")
+        }
+    }
+    
+    func buildForm() {
+        form +++
+            Section(){ section in
+                section.header = {
+                    var header = HeaderFooterView<UIView>(.callback({
+                        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.width))
+                        
+                        let imageView = UIImageView(image: self.savedImage)
+                        imageView.contentMode = .scaleAspectFit
+                        
+                        imageView.frame = CGRect(x: self.view.bounds.width/4, y: self.view.bounds.width/16, width: self.view.bounds.width/2, height: self.view.bounds.width/2)
+                        
+                        imageView.layer.cornerRadius = imageView.frame.width/2
+                        imageView.layer.masksToBounds = true
+                        imageView.layer.borderWidth = 0
+                        
+                        view.addSubview(imageView)
+                        
+                        return view
+                    }))
+                    
+                    header.height = { self.view.bounds.width/1.75 }
+                    return header
+                }()
+            }
+            
+            +++ Section("Account Information")
+            
+            <<< EmailRow(){
+                $0.title = "\(self.savedUsername)"
+                $0.disabled = true
+                $0.tag = "usernameRow"
+                $0.value = "\(savedEmail)"
+                }.cellUpdate { cell, row in
+                    row.title = "\(self.savedUsername)"
+            }
+            
+            +++ Section("Account Actions")
+            
+            <<< LabelRow(){
+                $0.title = "Change Email"
+                }.onCellSelection { _,_ in
+                    self.showTextInputPrompt(withMessage: "Email Address:") { (userPressedOK, userInput) in
+                        if let userInput = userInput {
+                            FIRAuth.auth()?.currentUser?.updateEmail(userInput) { (error) in
+                                self.showTypicalUIForUserUpdateResults(withTitle: "Change Email", error: error)
+                            }
+                        } else {
+                            self.createAlert(title: "Error", message: "Email cannot be empty")
+                        }
+                    }
+            }
+            
+            <<< LabelRow(){
+                $0.title = "Change Password"
+                }.onCellSelection { _,_ in
+                    self.showTextInputPrompt(withMessage: "New Password:") { (userPressedOK, userInput) in
+                        if let userInput = userInput {
+                            FIRAuth.auth()?.currentUser?.updatePassword(userInput) { (error) in
+                                self.showTypicalUIForUserUpdateResults(withTitle: "Change Password", error: error)
+                            }
+                        } else {
+                            self.createAlert(title: "Error", message: "Password cannot be empty")
+                        }
+                    }
+            }
+            
+            <<< LabelRow(){
+                $0.title = "Sign Out"
+                }.onCellSelection { _,_ in
+                    let firebaseAuth = FIRAuth.auth()
+                    do {
+                        try firebaseAuth?.signOut()
+                        self.performSegue(withIdentifier: "didSignOut", sender: self)
+                    } catch let signOutError as NSError {
+                        print ("Error signing out: %@", signOutError)
+                    }
+        }
+    }
+    
+    func firebaseLogin(_ credential: FIRAuthCredential) {
+        if let user = FIRAuth.auth()?.currentUser { // Linking Credentials
+            user.link(with: credential) { (user, error) in
+                if let error = error {
+                    self.createAlert(title: "Error", message: error.localizedDescription)
+                    return
+                }
+                self.getData()
+            }
+        } else {
+            self.createAlert(title: "Error", message: "Error, you are not currently logged in.")
+        }
+    }
 
+    func showTypicalUIForUserUpdateResults(withTitle resultsTitle: String, error: Error?) {
+        if let error = error {
+            let message = "\(error.localizedDescription)"
+            let okAction = UIAlertAction.init(title: "OK", style: .default) {
+                action in
+                // print("OK")
+            }
+            let alertController  = UIAlertController.init(title: resultsTitle,
+                                                          message: message, preferredStyle: .alert)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+    }
 }
