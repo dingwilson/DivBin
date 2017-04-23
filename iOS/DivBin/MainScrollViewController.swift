1b7d60039e52b08b6bfb9b223534772a106341d2
Wilson Ding <shodai100@gmail.com>
2017-04-23 01:16:49 -0500

    Added basic storyboard layout

---
+++ iOS/DivBin/MainScrollViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainScrollViewController.swift
+//  DivBin
+//
+//  Created by Wilson Ding on 4/23/17.
+//  Copyright © 2017 wilsonding. All rights reserved.
+//
+
+import UIKit
+
+class MainScrollViewController: UIViewController {
+    
+    private let vcInScrollView : [String] = ["ProfileView", "CameraView", "PledgeView"]
+    
+    private let indexOfInitialVC : CGFloat = 1 // Index of Initial VC to show
+    
+    override var prefersStatusBarHidden : Bool { // Hide Status Bar for CameraVC
+        return true
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupScrollView(viewcontrollers: vcInScrollView)
+    }
+    
+    func setupScrollView(viewcontrollers: [String]) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        for (index, identifier) in viewcontrollers.enumerated() {
+            let childVC = storyboard.instantiateViewController(withIdentifier: identifier)
+            
+            var childFrame = childVC.view.frame
+            childFrame.origin.x = self.view.frame.size.width * CGFloat(index)
+            childVC.view.frame = childFrame
+            
+            self.addChildViewController(childVC)
+            self.scrollView.addSubview(childVC.view)
+            childVC.didMove(toParentViewController: self)
+        }
+        
+        self.scrollView.contentSize = CGSize(width: self.view.frame.size.width * CGFloat(vcInScrollView.count), height: self.view.frame.size.height)
+        
+        self.scrollView.contentOffset = CGPoint(x: self.view.frame.size.width * indexOfInitialVC, y: 0)
+    }
+}
